82ab60e9de3b234ca8fd149aae898bc9460b71a2
Paul <iamtheodo@gmail.com>
2020-04-30 14:17:56 +1000

    Support for controllers / hands
    Controller avatars de-activate when hands are tracked (and vice versa)

---
+++ OculusHandTracking/Assets/HandMGR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMGR : MonoBehaviour
+{
+    public OVRHand leftHand;
+    public OVRHand rightHand;
+    public GameObject leftController;
+    public GameObject rightController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForHands();
+    }
+
+    private void CheckForHands()
+    {
+        if (leftHand.IsTracked || rightHand.IsTracked)
+        {
+            // Disable controllers if hands are tracked
+            leftController.SetActive(false);
+            rightController.SetActive(false);
+        }
+        else
+        {
+            // Enable controllers if hands are not tracked / controllers are activated
+            leftController.SetActive(true);
+            rightController.SetActive(true);
+        }
+    }
+}
